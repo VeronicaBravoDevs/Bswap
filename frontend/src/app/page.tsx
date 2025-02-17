444624fbf02689569f6d3e580032312fe7ac9516
Felipe Arce <arceprogramando@gmail.com>
2025-02-17 01:04:35 -0300

    feat: add animated book GIF to Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <>
+      <Image src="/bookgif.gif" width={500} height={500} alt="Fig de Personas Agarrando un libro" className="w-full" />
       <h2 className="text-2xl font-bold bg-red-100">Welcome to Bswap</h2>
-    </div>
+    </>
   );
 }
