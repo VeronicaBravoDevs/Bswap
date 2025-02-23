0a9e73a34457c01b93ea819c09841bb19106aea5
fernando valdes <valdesmartin@gmail.com>
2025-02-23 14:06:53 -0300

    fix: upload endpoint

--- backend/prisma/generated/client/edge.js
+++ backend/prisma/generated/client/edge.js
@@ -302,7 +302,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- backend/prisma/generated/client/index.js
+++ backend/prisma/generated/client/index.js
@@ -303,7 +303,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
