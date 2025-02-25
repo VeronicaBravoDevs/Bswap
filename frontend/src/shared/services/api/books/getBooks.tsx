1ddf7b7628a288f285f8f89f095ec8f572fcfe1f
Felipe Arce <arceprogramando@gmail.com>
2025-02-25 15:13:25 -0300

    feat: update getBooks function to fetch data from the live API and handle errors more gracefully

--- frontend/src/shared/services/api/books/getBooks.tsx
+++ frontend/src/shared/services/api/books/getBooks.tsx
@@ -1,13 +1,18 @@
-const URLMOCKDATA = "/mock/mockData.json";
-// const URLGETBOOKS = "https://equipo-s21-05-m-webapp.onrender.com/books";
-
 export const getBooks = async () => {
   try {
-    const response = await fetch(URLMOCKDATA);
-    if (!response.ok) throw new Error("No se cargan libros correctamente");
-    const data = await response.json();
-    return data;
+    const res = await fetch("https://equipo-s21-05-m-webapp.onrender.com/books");
+
+    if (!res.ok) {
+      throw new Error(`Error fetching books: ${res.status} ${res.statusText}`);
+    }
+
+    const { data } = await res.json();
+
+    console.log("✅ ~ getBooks ~ data:", data);
+
+    return data ?? [];
   } catch (error) {
-    return error;
+    console.error("❌ ~ getBooks ~ error:", error);
+    return [];
   }
 };
