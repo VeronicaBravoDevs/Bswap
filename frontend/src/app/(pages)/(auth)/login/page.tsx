c82ec7db4c6d85d8941c25960a154b9d5c328f6f
Veronica Bravo <verobravo112012@gmail.com>
2025-03-02 20:43:46 -0300

    feat: install react icons

--- frontend/src/app/(pages)/(auth)/login/page.tsx
+++ frontend/src/app/(pages)/(auth)/login/page.tsx
@@ -1,10 +1,48 @@
-import { Metadata } from 'next';
+import { Button } from "@/shared/components/ui/Button";
+import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Bswap - Logearse',
-  description: 'Logeate a Bswap y comienza a intercambiar tus libros en tu ciudad.',
+  title: "Bswap - Logearse",
+  description:
+    "Logeate a Bswap y comienza a intercambiar tus libros en tu ciudad.",
 };
 
 export default function Login() {
-  return <h2>Login</h2>;
+  return;
+  <>
+  <div className="border-2 border-gray-500 p-8">
+    <div>
+      <h1 className="font-bold text-center">Iniciar sesion en Bswap</h1>
+      <h2 className="text-center leading-7">
+        Comparte libros, <br /> descubre nuevas historias{" "}
+      </h2>
+    </div>
+    <div>
+      <Button className="bg-blue-700 text-white p-6">continuar con correo electronico</Button>
+    </div>
+
+    <p> - o - </p>
+    <div>
+      <Button className="font-bold text-center p-9">        
+        <span>🟢</span>Continuar con google
+      </Button>
+      <Button className="font-bold text-center p-9">    
+        <span>🟢</span> Continuar con Facebook
+      </Button>
+    </div>
+    <div>
+      <p>
+        No tenes cuenta en Bswap <Link>Registrate</Link>
+      </p>
+    </div>
+
+    <div>
+      <p>
+        Al continuar acepta nuestros <Link>Terminos de servicio</Link> y{" "}
+        <Link>Politica de privacidad</Link>
+      </p>
+    </div>
+    </div>
+  </>;
 }
