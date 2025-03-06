5e5939a57666249ba3df0f18a133019c2accca91
Felipe Arce <arceprogramando@gmail.com>
2025-03-06 10:33:23 -0300

    feat: add new image domain to Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['res.cloudinary.com'], // Añade aquí los dominios de tus imágenes
+    domains: ["res.cloudinary.com", "equipo-s21-05-m-webapp.onrender.com"],
   },
 };
 
