75dbb25a0987a9af1b2a43a9817997b48f381d67
Felipe Arce <arceprogramando@gmail.com>
2025-03-10 07:21:36 -0300

    feat: improve design in 'Mi biblioteca'

--- frontend/src/app/(pages)/(user)/mylibrary/page.tsx
+++ frontend/src/app/(pages)/(user)/mylibrary/page.tsx
@@ -1,38 +1,77 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function MyLibrary() {
   return (
     <>
-      <h2 className="text-center  text-4xl font-bold">Mi Biblioteca</h2>
+      <h2 className="text-center  text-4xl font-bold my-8">Mi Biblioteca</h2>
 
-      <div className="bg-white p-4 rounded-md">
-        <p className="text-bold text-xl font-bold">Estas en tu cuenta de Bswap</p>
-        <h3>Crear Libros</h3>
-        <p>¿Necesitas cambiar tu cuenta?</p>{" "}
-        <Link href={"/logout"}>Cerrar Sesión</Link>
-        <nav className="flex-wrap">
-          <ul className="flex flex-wrap gap-4">
-            <li className="underline">Datos Personales</li>
-            <li className="underline">Mi biblioteca</li>
-            <li className="underline">Mis seguidores</li>
+      <div className="bg-white border-blue-800 p-4 rounded-md max-w-4xl mx-auto">
+        <p className="font-bold text-xl">Estas en tu cuenta de Bswap</p>
+        <div className="flex my-2 gap-2">
+          <p>¿Necesitas cambiar tu cuenta?</p>{" "}
+          <Link href={"/logout"} className="underline">
+            Cerrar Sesión
+          </Link>
+        </div>
+        <nav>
+          <ul className="flex gap-4 [&>li]:underline [&>li]:font-bold my-4">
+            <li>Datos Personales</li>
+            <li className="bg-yellow-200">Mi biblioteca</li>
+            <li>Mis seguidores</li>
           </ul>
 
           <div>
-            <p>Mis Libros</p>
+            <div className="flex items-center border border-blue-400 p-2">
+              <Image
+                src="/booksEmoji.png"
+                alt="emoji de libros"
+                width={30}
+                height={30}
+              />
+              <p className="font-bold text-xl">Mis Libros</p>
+            </div>
+            <p className="py-4">¡Tu Biblioteca esta vacia!</p>
           </div>
           <div>
-            <p>Libros Intercambiados</p>
+            <div className="flex items-center border border-blue-400 p-2">
+              <Image
+                src="/orangeBookEmoji.png"
+                alt="emoji de libros"
+                width={30}
+                height={30}
+              />
+              <p className="font-bold text-xl">Libros Intercambiados</p>
+            </div>
+            <p className="py-4">¡Tu Biblioteca esta vacia!</p>
           </div>
           <div>
-            <p>Libros Que deseo</p>
+            <div className="flex items-center border border-blue-400 p-2">
+              <Image
+                src="/bookStarEmoji.png"
+                alt="emoji de libros"
+                width={30}
+                height={30}
+              />
+              <p className="font-bold text-xl">Libros que deseo</p>
+            </div>
+            <p className="py-4">¡Tu Biblioteca esta vacia!</p>
           </div>
           <div>
-            <Link href="/books/createbook">Cargar Libro</Link>
+            <div className="flex items-center border border-blue-400 p-2">
+              <Image
+                src="/uploadImageButton.png"
+                alt="emoji de libros"
+                width={30}
+                height={30}
+              />
+              <Link href="/books/createbook" className="font-bold text-xl">
+                Cargar Libro
+              </Link>
+            </div>
           </div>
         </nav>
       </div>
-
-      <Link href="/books/createbook">Subir Libro</Link>
     </>
   );
 }
