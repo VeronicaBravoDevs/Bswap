3d020f201305fdc837e0264988c34c98dc4883b7
Veronica Bravo <verobravo112012@gmail.com>
2025-03-12 06:11:50 -0300

    feat: add community board picture

--- frontend/src/app/(pages)/community/page.tsx
+++ frontend/src/app/(pages)/community/page.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 export default function Community() {
   return (
-    <>
-      <h1>Community</h1>
+<>
+    <h1 className="text-4xl font-bold my-8 text-center">
+    Comunidad
+  </h1>
+
+
+    <div className="relative w-screen h-screen">
+      <Image 
+        src="/pizarra_de_libros.png" 
+        alt="Imagen de pizarra de eventos" 
+        layout="fill" 
+        objectFit="cover" 
+        priority
+      />
+   
+    </div>
     </>
   );
 }
