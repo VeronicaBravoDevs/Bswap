ed46250544f878ffda95125ff8431fa0c065faff
Felipe Arce <arceprogramando@gmail.com>
2025-03-12 00:28:24 -0300

    test: test image error in frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,20 @@
 const nextConfig = {
   images: {
+    domains: ["res.cloudinary.com", "equipo-s21-05-m-webapp.onrender.com"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'equipo-s21-05-m-webapp.onrender.com',
+        protocol: "https",
+        hostname: "equipo-s21-05-m-webapp.onrender.com",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '4000',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/uploads/**",
       },
     ],
   },
